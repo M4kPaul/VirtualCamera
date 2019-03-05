0653098201177fbcd4d2c18b4a99cdd0d93787af
Paweł Makowski <m4kpaul@gmail.com>
2019-03-05 03:51:13 +0100

    Replaced custom function with build in method

--- VirtualCamera/MainWindow.cs
+++ VirtualCamera/MainWindow.cs
@@ -14,9 +14,8 @@ namespace VirtualCamera
         private float yaw;
         private float roll;
         private Vector3 camera;
-        private Vector3 lookDir;
         private bool isSolid;
-        private Matrix4x4 rotZXMat;
+        //private Matrix4x4 rotZXMat;
 
         public MainWindow()
         {
@@ -33,26 +32,24 @@ namespace VirtualCamera
             yaw = 0.0F;
             roll = 0.0F;
             camera = new Vector3(0, 0, 0);
-            lookDir = new Vector3(0, 0, 1);
             isSolid = false;
-            rotZXMat = Matrix4x4.Multiply(Transformations.RotateZ(0), Transformations.RotateX(0));
         }
 
         private void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Up) camera = Vector3.Add(camera, lookDir);
-            if (e.KeyCode == Keys.Down) camera = Vector3.Subtract(camera, lookDir);
-            if (e.KeyCode == Keys.Left) camera.X += 0.5F;
-            if (e.KeyCode == Keys.Right) camera.X -= 0.5F;
-            if (e.KeyCode == Keys.U) camera.Y += 0.5F;
-            if (e.KeyCode == Keys.J) camera.Y -= 0.5F;
+            if (e.KeyCode == Keys.Up) camera.Z -= 0.5F;
+            if (e.KeyCode == Keys.Down) camera.Z += 0.5F;
+            if (e.KeyCode == Keys.Left) camera.X -= 0.5F;
+            if (e.KeyCode == Keys.Right) camera.X += 0.5F;
+            if (e.KeyCode == Keys.U) camera.Y -= 0.5F;
+            if (e.KeyCode == Keys.J) camera.Y += 0.5F;
 
-            if (e.KeyCode == Keys.W) pitch -= 0.1F;
-            if (e.KeyCode == Keys.S) pitch += 0.1F;
-            if (e.KeyCode == Keys.A) yaw -= 0.1F;
-            if (e.KeyCode == Keys.D) yaw += 0.1F;
-            if (e.KeyCode == Keys.Q) roll += 0.1F;
-            if (e.KeyCode == Keys.E) roll -= 0.1F;
+            if (e.KeyCode == Keys.W) pitch += 0.1F;
+            if (e.KeyCode == Keys.S) pitch -= 0.1F;
+            if (e.KeyCode == Keys.A) yaw += 0.1F;
+            if (e.KeyCode == Keys.D) yaw -= 0.1F;
+            if (e.KeyCode == Keys.Q) roll -= 0.1F;
+            if (e.KeyCode == Keys.E) roll += 0.1F;
 
             if (e.KeyCode == Keys.OemMinus) fov += 1.0F;
             if (e.KeyCode == Keys.Oemplus) fov -= 1.0F;
@@ -74,53 +71,41 @@ namespace VirtualCamera
             var far = 1000.0F;
             var projMat = new Matrix4x4
             {
-                M11 = aspectRatio * fovRad,
+                M11 = fovRad * aspectRatio,
                 M22 = fovRad,
                 M33 = far / (far - near),
-                M43 = (-far * near) / (far - near),
-                M34 = 1.0F
+                M34 = 1.0F,
+                M43 = (-near * far) / (far - near)
             };
 
-            var up = new Vector3(0, 1, 0);
-            var target = new Vector3(0, 0, 1);
-            var rotYMat = Transformations.RotateY(yaw);
-            lookDir = Transformations.V4ToV3(Transformations.MultiplyMatVect(rotYMat, new Vector4(target, 1.0F)));
-            target = Vector3.Add(camera, lookDir);
-            var cameraMat = Transformations.PointAt(camera, target, up);
-            var viewMat = Transformations.QuickInverse(cameraMat);
-
-            var rotZMat = Transformations.RotateZ(roll);
             var rotXMat = Transformations.RotateX(pitch);
-            rotZXMat = Matrix4x4.Multiply(rotZMat, rotXMat);
-            var worldTransMat = Transformations.Translate(0.0F, -5.0F, 15.0F);
-            worldTransMat = Matrix4x4.Multiply(rotZXMat, worldTransMat);
+            var rotYMat = Transformations.RotateY(yaw);
+            //var rotZMat = Transformations.RotateZ(roll);
+            var rotYXMat = Matrix4x4.Multiply(rotYMat, rotXMat);
+            var worldTransMat = Transformations.Translate(camera.X, camera.Y - 5.0F, camera.Z + 15.0F);
+            worldTransMat = Matrix4x4.Multiply(rotYXMat, worldTransMat);
             var projMesh = new List<Vector4[]>();
             foreach (var triangle in worldData.Mesh)
             {
-                var transformedTri = new Vector4[3];
-                transformedTri[0] = Transformations.MultiplyMatVect(worldTransMat, triangle[0]);
-                transformedTri[1] = Transformations.MultiplyMatVect(worldTransMat, triangle[1]);
-                transformedTri[2] = Transformations.MultiplyMatVect(worldTransMat, triangle[2]);
+                var transTri = new Vector4[3];
+                transTri[0] = Vector4.Transform(triangle[0], worldTransMat);
+                transTri[1] = Vector4.Transform(triangle[1], worldTransMat);
+                transTri[2] = Vector4.Transform(triangle[2], worldTransMat);
 
                 if (isSolid)
                 {
-                    Vector3 point0 = Transformations.V4ToV3(transformedTri[0]);
-                    Vector3 line1 = Vector3.Subtract(Transformations.V4ToV3(transformedTri[1]), point0);
-                    Vector3 line2 = Vector3.Subtract(Transformations.V4ToV3(transformedTri[2]), point0);
+                    Vector3 point0 = Transformations.V4ToV3(transTri[0]);
+                    Vector3 line1 = Vector3.Subtract(Transformations.V4ToV3(transTri[1]), point0);
+                    Vector3 line2 = Vector3.Subtract(Transformations.V4ToV3(transTri[2]), point0);
                     Vector3 normal = Vector3.Normalize(Vector3.Cross(line1, line2));
 
                     if (Vector3.Dot(normal, Vector3.Subtract(point0, camera)) >= 0.0F) continue;
                 }
 
-                var viewedTri = new Vector4[3];
-                viewedTri[0] = Transformations.MultiplyMatVect(viewMat, transformedTri[0]);
-                viewedTri[1] = Transformations.MultiplyMatVect(viewMat, transformedTri[1]);
-                viewedTri[2] = Transformations.MultiplyMatVect(viewMat, transformedTri[2]);
-
                 var projTri = new Vector4[3];
-                projTri[0] = Transformations.MultiplyMatVect(projMat, viewedTri[0]);
-                projTri[1] = Transformations.MultiplyMatVect(projMat, viewedTri[1]);
-                projTri[2] = Transformations.MultiplyMatVect(projMat, viewedTri[2]);
+                projTri[0] = Vector4.Transform(transTri[0], projMat);
+                projTri[1] = Vector4.Transform(transTri[1], projMat);
+                projTri[2] = Vector4.Transform(transTri[2], projMat);
 
                 projTri[0] = Vector4.Divide(projTri[0], projTri[0].W);
                 projTri[1] = Vector4.Divide(projTri[1], projTri[1].W);
@@ -141,7 +126,7 @@ namespace VirtualCamera
                 projMesh.Add(projTri);
             }
 
-            projMesh.Sort(Transformations.CompareTriangles);
+            if (isSolid) projMesh.Sort(Transformations.CompareTriangles);
             var pen = new Pen(Color.White, 1);
             var brush = new SolidBrush(Color.Gray);
             foreach (var projTri in projMesh)
@@ -152,9 +137,9 @@ namespace VirtualCamera
                    new PointF(projTri[1].X, projTri[1].Y),
                    new PointF(projTri[2].X, projTri[2].Y)
                 };
-                //graphics.DrawLine(pen, new PointF(projTri.Vertices[0].X, projTri.Vertices[0].Y), new PointF(projTri.Vertices[1].X, projTri.Vertices[1].Y));
-                //graphics.DrawLine(pen, new PointF(projTri.Vertices[0].X, projTri.Vertices[0].Y), new PointF(projTri.Vertices[2].X, projTri.Vertices[2].Y));
-                //graphics.DrawLine(pen, new PointF(projTri.Vertices[1].X, projTri.Vertices[1].Y), new PointF(projTri.Vertices[2].X, projTri.Vertices[2].Y));
+                //graphics.DrawLine(pen, new PointF(projTri[0].X, projTri[0].Y), new PointF(projTri[1].X, projTri[1].Y));
+                //graphics.DrawLine(pen, new PointF(projTri[0].X, projTri[0].Y), new PointF(projTri[2].X, projTri[2].Y));
+                //graphics.DrawLine(pen, new PointF(projTri[1].X, projTri[1].Y), new PointF(projTri[2].X, projTri[2].Y));
                 try
                 {
                     graphics.DrawPolygon(pen, projPoints);
--- VirtualCamera/Transformations.cs
+++ VirtualCamera/Transformations.cs
@@ -5,19 +5,6 @@ namespace VirtualCamera
 {
     static class Transformations
     {
-        public static Vector4 MultiplyMatVect(Matrix4x4 mat, Vector4 vect)
-        {
-            Vector4 res = new Vector4
-            {
-                X = vect.X * mat.M11 + vect.Y * mat.M21 + vect.Z * mat.M31 + vect.W * mat.M41,
-                Y = vect.X * mat.M12 + vect.Y * mat.M22 + vect.Z * mat.M32 + vect.W * mat.M42,
-                Z = vect.X * mat.M13 + vect.Y * mat.M23 + vect.Z * mat.M33 + vect.W * mat.M43,
-                W = vect.X * mat.M14 + vect.Y * mat.M24 + vect.Z * mat.M34 + vect.W * mat.M44
-            };
-
-            return res;
-        }
-
         public static Matrix4x4 Translate(float x, float y, float z)
         {
             Matrix4x4 res = new Matrix4x4
@@ -40,8 +27,8 @@ namespace VirtualCamera
             {
                 M11 = 1.0F,
                 M22 = (float)Math.Cos(radAngle),
-                M23 = (float)Math.Sin(radAngle),
-                M32 = -(float)Math.Sin(radAngle),
+                M23 = -(float)Math.Sin(radAngle),
+                M32 = (float)Math.Sin(radAngle),
                 M33 = (float)Math.Cos(radAngle),
                 M44 = 1.0F
             };
@@ -69,8 +56,8 @@ namespace VirtualCamera
             Matrix4x4 res = new Matrix4x4
             {
                 M11 = (float)Math.Cos(radAngle),
-                M12 = (float)Math.Sin(radAngle),
-                M21 = -(float)Math.Sin(radAngle),
+                M12 = -(float)Math.Sin(radAngle),
+                M21 = (float)Math.Sin(radAngle),
                 M22 = (float)Math.Cos(radAngle),
                 M33 = 1.0F,
                 M44 = 1.0F
@@ -94,37 +81,6 @@ namespace VirtualCamera
                 Y = v.Y,
                 Z = v.Z
             };
-
-            return res;
-        }
-
-        public static Matrix4x4 PointAt(Vector3 pos, Vector3 target, Vector3 up)
-        {
-            Vector3 newForward = Vector3.Normalize(Vector3.Subtract(target, pos));
-            Vector3 newUp = Vector3.Normalize(Vector3.Subtract(up, Vector3.Multiply(newForward, Vector3.Dot(up, newForward))));
-            Vector3 newRight = Vector3.Cross(newUp, newForward);
-
-            Matrix4x4 res = new Matrix4x4(
-                      newRight.X,   newRight.Y,   newRight.Z, 0.0F,
-                         newUp.X,      newUp.Y,      newUp.Z, 0.0F,
-                    newForward.X, newForward.Y, newForward.Z, 0.0F,
-                           pos.X,        pos.Y,        pos.Z, 1.0F
-                );
-
-            return res;
-        }
-        
-        public static Matrix4x4 QuickInverse(Matrix4x4 m) // trans/rot only!
-        {
-            Matrix4x4 res = new Matrix4x4(
-                    m.M11, m.M21, m.M31, 0.0F,
-                    m.M12, m.M22, m.M32, 0.0F,
-                    m.M13, m.M23, m.M33, 0.0F,
-                    -(m.M41 * m.M11 + m.M42 * m.M21 + m.M43 * m.M31),
-                    -(m.M41 * m.M12 + m.M42 * m.M22 + m.M43 * m.M32),
-                    -(m.M41 * m.M13 + m.M42 * m.M23 + m.M43 * m.M33),
-                    1.0F
-                );
 
             return res;
         }
